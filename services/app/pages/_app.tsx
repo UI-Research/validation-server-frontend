570471344cc7379ac5430a883ff1e77444a6ce5f
Michael Drayer <mdrayer@forumone.com>
2021-06-22 11:37:38 -0400

    [UITX21-1] Install and configure Material UI.

--- services/app/pages/_app.tsx
+++ services/app/pages/_app.tsx
@@ -1,12 +1,21 @@
-import { ThemeProvider, Global } from '@emotion/react';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import type { AppProps } from 'next/app';
-import createBaseStyles from '../styles/createBaseStyles';
-import theme from '../styles/theme';
+import React from 'react';
+import theme from '../styles/material-ui/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-      <Global styles={createBaseStyles(theme)} />
+      <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- services/app/pages/index.tsx
+++ services/app/pages/index.tsx
@@ -1,40 +1,137 @@
-import styled from '@emotion/styled';
-import Head from 'next/head';
+import PageTemplate from '../components/PageTemplate';
 
-// Just an example of emotion working.
-const H1 = styled('h1')`
-  color: ${props => props.theme.palette.blue.base};
-`;
+const title = 'UI-Research/validation-server-frontend';
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>UI-Research/validation-server-frontend</title>
-        <meta
-          name="description"
-          content="UI-Research/validation-server-frontend"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <PageTemplate title={title}>
+      <h1>{title}</h1>
+      <div>
+        <p>
+          Alias adipisci, dictum dictumst in aliquam facere velit? Iste purus
+          lectus delectus et, facilis tincidunt facere exercitation error.
+          Proident mollis itaque ullamco odit. Sapiente hac, mauris voluptas
+          excepturi magna nibh. Culpa, convallis egestas sollicitudin? Conubia?
+          Cupidatat! Dolor nulla lacus, qui, nostrum aspernatur, mollit eros.
+          Orci adipisicing incididunt dapibus? Dicta iaculis sociis aperiam
+          veritatis? Vestibulum, nostra, dignissimos nonummy harum? Id et.
+        </p>
 
-      <main>
-        <H1>UI-Research/validation-server-frontend</H1>
+        <p>
+          Fringilla anim? Consectetur? Tenetur repellat distinctio, faucibus,
+          accumsan adipisci dolore quia fermentum blandit, a vehicula dictumst
+          pretium atque. Necessitatibus. Repellendus, ab, corrupti beatae sit
+          felis eum est montes! Purus, risus earum tempor temporibus qui nec
+          montes, dis eius tempus! Veritatis. Mi architecto, adipisicing
+          consequuntur mauris iste? Temporibus, aliqua neque veniam! Doloremque
+          vehicula, phasellus magnis minima vestibulum eleifend class porta
+          dignissimos.
+        </p>
 
-        <div>
-          Gravida voluptates eleifend adipisci? Diamlorem rutrum penatibus
-          earum, quasi feugiat quo ligula! Primis. Dolores etiam diamlorem fusce
-          cum reprehenderit orci. Bibendum habitant, praesentium habitasse
-          senectus possimus. Nostrum? Fringilla. Maxime pretium, lacinia velit.
-          Atque arcu sapiente corrupti? Beatae mattis rerum eum.
-        </div>
-      </main>
+        <p>
+          Wisi vitae numquam vero, repellendus penatibus incidunt? Aperiam
+          assumenda eaque ipsa, commodi distinctio quam! Eligendi ligula duis
+          mattis mi esse integer a incidunt sem. Mus aptent deleniti etiam. Wisi
+          animi soluta sodales doloremque curae elit? Natus ridiculus corporis
+          minima pretium! Reiciendis laboris quis convallis porttitor. Eros,
+          magna semper, minima arcu! Conubia odit, proin earum, eros porro
+          nonummy! Inceptos magni, dolorem.
+        </p>
 
-      <footer>
-        <div>
-          Omnis impedit dapibus rem amet tempus, massa habitasse nonummy aut.
-        </div>
-      </footer>
-    </div>
+        <p>
+          Torquent error, exercitationem eaque wisi felis ducimus dictum.
+          Quaerat veniam. Diam mattis, non iusto penatibus non, nulla
+          praesentium autem eiusmod culpa quia delectus porro fuga, imperdiet
+          et? Quis donec quisquam. Fusce voluptatibus duis hic laboriosam anim
+          ullamcorper dignissimos, aspernatur, illum impedit natoque aute
+          ratione iusto quisquam laboriosam sint porro diamlorem facilis
+          tenetur. Parturient inventore adipiscing ligula eu nisi. Aliquid
+          necessitatibus.
+        </p>
+
+        <p>
+          Cras purus! Repudiandae aute dolorem quae magnis ut nostrud ipsum?
+          Explicabo cum habitasse autem nostrum tempore proident animi sit
+          rutrum interdum, repellat molestiae tellus, mollit fames ligula illo,
+          maxime voluptate, cumque torquent omnis ultrices? Praesent
+          consectetuer, odit, sed non nibh semper aenean nam per, porro possimus
+          dolore inventore ultricies sociosqu, accusantium, curabitur habitant
+          congue posuere eos dapibus quisquam pretium orci.
+        </p>
+
+        <p>
+          Commodo pretium ipsa! Proident, exercitationem aenean quibusdam ipsam,
+          placeat, nisl. Minim placeat dis aliqua duis, placerat velit quis,
+          voluptatibus natus parturient malesuada phasellus cupiditate! Metus
+          class nostrud, accusantium natus aenean, pede laborum, dolorem,
+          doloribus magna, sollicitudin, proin dapibus fermentum modi diam quo
+          vel donec, donec laoreet doloremque vehicula, augue tristique est.
+          Vero voluptatibus rutrum purus felis urna blandit? Occaecat quisquam.
+        </p>
+
+        <p>
+          Unde accumsan tellus, amet augue anim, lacinia cursus exercitationem
+          exercitationem. Repellat, ut! Varius qui suscipit qui molestiae
+          posuere ante nisi totam pede cumque. Per porttitor auctor nostra ipsa
+          malesuada explicabo tortor et, aenean nibh doloribus ipsa, fermentum
+          ullamcorper iusto! Commodi voluptas aspernatur aperiam dictum, quae
+          expedita, cubilia ullamcorper. Tenetur totam laborum illum! Habitant
+          taciti tincidunt inceptos integer aliquet maecenas iure.
+        </p>
+
+        <p>
+          Tenetur ultricies bibendum adipisci, pharetra adipiscing suscipit
+          exercitation eiusmod inventore nonummy maiores, consequat lacus
+          cillum? Qui possimus cubilia ipsa aliquet aute minus, iure? Eleifend,
+          ea cillum iusto augue. Hendrerit laborum officiis imperdiet praesent
+          placerat maxime laborum. Non morbi, sed quaerat iure eros voluptate
+          tristique primis sequi class inventore, interdum nisl accumsan. Unde
+          minim pellentesque phasellus quibusdam expedita incidunt quae
+          fermentum.
+        </p>
+
+        <p>
+          Vitae laoreet perferendis, cillum quisque asperiores, rutrum accumsan
+          nemo mauris? Leo suscipit repellendus proident, similique odio primis
+          penatibus optio consequuntur, excepteur, aptent diam molestias? Rerum
+          doloribus voluptates! Scelerisque egestas minim! Molestias in, rerum
+          ullamcorper culpa vulputate! Cubilia fermentum laoreet! Ullamcorper
+          quos aute excepturi officiis phasellus. Mi fusce porro! Ipsam
+          exercitation, blanditiis cubilia, ipsa at, ante quisque, eget
+          repellendus laoreet sociosqu.
+        </p>
+
+        <p>
+          Rhoncus aenean dolor placeat hendrerit sed assumenda sapiente! Cursus
+          sapiente animi. Sociis gravida similique pharetra aliqua! Class
+          laboriosam iusto morbi. Cupidatat? Proident! Mus, occaecati similique
+          tellus laboriosam, molestiae! Quos, quis feugiat labore proin ipsam!
+          Vulputate congue. Vivamus deleniti justo assumenda et tellus beatae
+          ad? Deserunt? Odit magnis error. Nostrum reiciendis facilisis,
+          veritatis ullamcorper nihil aliquam do cubilia alias, hac corrupti.
+        </p>
+
+        <p>
+          Repellat tempora perspiciatis montes, nostrum duis, augue laudantium
+          ratione dis, fames ante nam quibusdam, taciti. Autem adipisicing
+          consequatur adipisicing distinctio consequat malesuada? Netus neque
+          hic blanditiis primis velit. Accusamus repellat fuga sociis, dictumst
+          venenatis. Debitis mus? Minim necessitatibus! Explicabo et quisque
+          quasi congue perferendis! Augue eaque maecenas magnam fermentum per
+          illum nisi, dignissim occaecati, sunt molestie placerat, consectetuer
+          optio metus.
+        </p>
+
+        <p>
+          Magna hac adipisci dapibus quis praesent dicta quam, eiusmod augue
+          interdum ducimus, facilisi ex? Magnis elementum parturient quia.
+          Parturient reprehenderit soluta minus. Mollitia quibusdam ab,
+          venenatis! Assumenda lacus. Auctor nibh, congue potenti! Earum. Minim
+          perspiciatis? Nobis labore esse, ea sed repellendus lobortis sit
+          adipisci modi, iste! Gravida provident cupiditate parturient cubilia
+          fermentum proident ante, sint nonummy iusto, lorem elementum per.
+        </p>
+      </div>
+    </PageTemplate>
   );
 }

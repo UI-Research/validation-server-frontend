c7f12b9e79be44972fe9628556c4c61f1a3b7408
Michael Drayer <mdrayer@forumone.com>
2021-09-15 12:50:52 -0400

    [UITX21-18] Install recharts. Create refine adjustments chart.

---
+++ services/app/components/ReviewRefine/RefineAdjustmentsChart.tsx
@@ -0,0 +1,91 @@
+import { Typography } from '@material-ui/core';
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { AccuracyData, Columns } from './RefineAdjustmentsDialog';
+
+function range(start: number, end: number): number[] {
+  if (start > end) {
+    throw new Error('start value is larger than end value.');
+  }
+  const arr: number[] = [];
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+const X_DOMAIN = [0, 10];
+const X_TICKS = range(1, 9);
+
+interface RefineAdjustmentsChartProps {
+  data: AccuracyData[];
+}
+function RefineAdjustmentsChart({
+  data,
+}: RefineAdjustmentsChartProps): JSX.Element {
+  const chartData = data.map(d => ({
+    ...d,
+    [Columns.PRIVACY_COST]: Number(d[Columns.PRIVACY_COST]),
+  }));
+  return (
+    <div>
+      <Typography align="center">
+        <strong>Privacy Error vs. Privacy Cost</strong>
+      </Typography>
+      <ResponsiveContainer width="100%" height={325}>
+        <LineChart data={chartData} margin={{ bottom: 20 }}>
+          <XAxis
+            dataKey={Columns.PRIVACY_COST}
+            domain={X_DOMAIN}
+            ticks={X_TICKS}
+            tickLine={false}
+            type="number"
+            label={{
+              value: Columns.PRIVACY_COST,
+              position: 'bottom',
+              offset: 0,
+            }}
+          />
+          <YAxis
+            tickFormatter={n =>
+              typeof n === 'number' ? n.toLocaleString() : n
+            }
+            tickLine={false}
+            type="number"
+            label={{
+              value: Columns.PRIVACY_ERROR,
+              angle: -90,
+              position: 'insideLeft',
+              style: {
+                textAnchor: 'middle',
+              },
+            }}
+          />
+          <Line
+            type="monotone"
+            dataKey={'10'}
+            stroke="#16d2b0"
+            dot={false}
+            connectNulls={true}
+          />
+          <Line
+            type="monotone"
+            dataKey={'90'}
+            stroke="#1638d2"
+            dot={false}
+            connectNulls={true}
+          />
+          <Line
+            type="monotone"
+            dataKey={Columns.PRIVACY_ERROR}
+            stroke="#1696d2"
+            dot={false}
+            connectNulls={true}
+            strokeWidth={2}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default RefineAdjustmentsChart;

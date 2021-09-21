af236a116e09d67fb95ac7ac613e4e8f72dbb3f9
Michael Drayer <mdrayer@forumone.com>
2021-09-21 09:37:38 -0400

    [N/A] Move styleguide to its own page. Move logo to own component. Cleanup imports.

--- services/app/pages/data.tsx
+++ services/app/pages/data.tsx
@@ -1,6 +1,6 @@
 import PageTemplate from '../components/PageTemplate';
+import Paragraph from '../components/Paragraph';
 import SectionTitle from '../components/SectionTitle';
-import StyleGuide from '../components/StyleGuide/StyleGuide';
 
 const title = 'Data';
 
@@ -8,7 +8,110 @@ function DataPage(): JSX.Element {
   return (
     <PageTemplate title={title}>
       <SectionTitle>{title}</SectionTitle>
-      <StyleGuide />
+      <div>
+        <Paragraph>
+          Eros eleifend quisquam faucibus, aliquip fugit magna, laoreet
+          perspiciatis fuga! Cupidatat dignissim, aute habitasse reiciendis
+          faucibus minim, convallis, incidunt faucibus atque nam? Unde rem
+          torquent tempus torquent mus. Aenean sapiente, lacus ultrices.
+          Nesciunt similique suspendisse feugiat? Qui pellentesque, accusantium.
+          Nostrum, dolore totam, adipiscing aliquid interdum, aspernatur natus
+          nostrum, eligendi vehicula, sagittis numquam mi. Cursus exercitationem
+          reprehenderit, diamlorem, senectus dolorem adipiscing.
+        </Paragraph>
+
+        <Paragraph>
+          Voluptates do fugiat beatae velit voluptate leo natus ridiculus
+          excepteur reiciendis ultricies leo vivamus doloremque, dolores autem
+          illo, repellendus vitae commodo magnam sapiente consectetur,
+          voluptatibus? Risus varius vel? Culpa! Magna, dolorum netus doloribus
+          porttitor inceptos doloremque irure labore, qui. Laoreet dictumst
+          quia? Posuere suscipit class. Exercitationem quidem pede! Quaerat
+          reiciendis! Proident? Hymenaeos. Corrupti viverra quidem! Urna
+          pulvinar! Eum necessitatibus nonummy.
+        </Paragraph>
+
+        <Paragraph>
+          Nisi velit, ligula sunt exercitationem. Iusto veritatis? Itaque beatae
+          error sed reprehenderit esse dolor semper maxime molestiae? Iste etiam
+          justo mattis congue elit recusandae eget molestias? Et! Tenetur et
+          necessitatibus, arcu perferendis sodales nemo vestibulum aliquet
+          habitant repudiandae exercitationem integer debitis, praesent sed,
+          aspernatur, mus? Commodo, doloremque cubilia aut nostrum ducimus elit
+          maecenas voluptatum? Tincidunt leo dolorum malesuada? Nostra ipsam.
+        </Paragraph>
+
+        <Paragraph>
+          Condimentum aut cupidatat beatae aliquip semper corrupti phasellus
+          turpis eleifend nulla placeat dolore wisi proident amet nostra pede,
+          do ligula molestiae, excepturi. Facilisi nonummy nemo incididunt, mus!
+          Adipisicing do ultricies, pulvinar esse! Semper nostrud quo enim, ex
+          et nulla, harum tristique feugiat facilisi natoque sociosqu ullamco
+          iure conubia. Posuere facere! Quis tempora diam earum unde fugiat quo
+          nulla curabitur condimentum.
+        </Paragraph>
+
+        <Paragraph>
+          Quisquam pulvinar! Libero hymenaeos auctor nascetur lectus iusto?
+          Tellus ab exercitation ultrices neque faucibus, maecenas magna dolor
+          deleniti velit auctor. Viverra! Venenatis rhoncus lobortis similique
+          ducimus adipisicing nesciunt, nihil qui totam eligendi, semper nisi.
+          Consequat mauris sed fuga, accumsan nihil lobortis habitant, sociosqu,
+          tenetur wisi, exercitation veniam maiores. Urna facilisi pellentesque
+          error ducimus! Mollitia nisi per, ligula atque fames ac.
+        </Paragraph>
+
+        <Paragraph>
+          Euismod voluptatum omnis optio debitis cupidatat wisi mollit ornare
+          reiciendis duis eget, voluptatum, vehicula diam quas eaque eos officia
+          nesciunt condimentum optio potenti vero. Dolorem laoreet, ea urna
+          delectus aute euismod voluptatem rutrum deserunt sapiente cupiditate
+          architecto pretium assumenda integer. Netus quos, duis dictumst
+          impedit. Quaerat ullamco, odio! Aptent quam, neque itaque omnis alias
+          placerat feugiat? Fermentum nostrum porta sollicitudin.
+        </Paragraph>
+
+        <Paragraph>
+          Porta conubia, voluptatibus quas, euismod condimentum arcu malesuada.
+          Perferendis per class, rem laudantium aperiam, aliquid excepturi
+          mollitia aute architecto ullam earum? Ridiculus habitasse numquam
+          euismod ab leo curae! Dui eget incidunt omnis, gravida hendrerit!
+          Eaque elementum, natus odit sapiente, proin massa lectus minima tempor
+          impedit netus. Fames sint? Lacus aptent, id posuere, tellus, suscipit
+          et iste volutpat reprehenderit? Atque blandit.
+        </Paragraph>
+
+        <Paragraph>
+          Quam perspiciatis taciti duis augue tellus rhoncus sociis! Inventore
+          montes parturient earum, natoque vestibulum magna, dictumst eaque
+          mauris, earum deleniti? Mollit incididunt lorem morbi aenean
+          repudiandae tempor, rutrum! Proident rutrum consectetur, totam non,
+          consectetur consequatur quam veritatis voluptatibus aptent, sociosqu
+          venenatis bibendum inceptos culpa? Hendrerit? Unde soluta donec,
+          cubilia aperiam, auctor posuere, aptent nemo, alias parturient,
+          aliquam provident sem illum.
+        </Paragraph>
+
+        <Paragraph>
+          Rem hendrerit? Doloribus vulputate dapibus nostra, elit ipsum?
+          Aliquid. Semper earum proin, morbi aliqua. Integer massa temporibus,
+          ipsa diam voluptates laoreet natus? Eveniet optio dicta eligendi
+          litora aliquet, dictum nisl incididunt nostrum a felis pellentesque
+          felis penatibus blandit etiam proin voluptatum lacus? Odit quia,
+          netus! Eiusmod? Egestas augue imperdiet aperiam. Libero, voluptatem
+          suscipit repudiandae vestibulum varius hendrerit diam mus accumsan.
+        </Paragraph>
+
+        <Paragraph>
+          Laborum quae sequi scelerisque mi auctor proident nostrud doloremque
+          ex aliquip sagittis, lobortis minima, litora, imperdiet nam in,
+          habitant nisl. Nisl eleifend fermentum illum, venenatis nisi nobis
+          dapibus. Voluptate habitasse duis totam. Et pharetra leo parturient!
+          Unde primis reprehenderit, non! Cubilia ipsum praesent expedita ipsa?
+          Curabitur eleifend pretium, ornare, rhoncus distinctio minima, magnis
+          massa eu quidem aperiam pariatur tellus cupiditate.
+        </Paragraph>
+      </div>
     </PageTemplate>
   );
 }
